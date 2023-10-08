e2d0819c12badc2b4343f10fe2d1f4fa2287a90f
Jackson Abney <jackson.abney@northwesterndevelopment.com>
2023-10-07 17:56:17 -0800

    New

--- scripts/macros.js
+++ scripts/macros.js
@@ -1,3 +1,11 @@
+console.log("Hello World! This code runs immediately when the file is loaded.");
+
+Hooks.on("init", function () {
+    console.log(
+        "This code runs once the Foundry VTT software begins its initialization workflow."
+    );
+});
+
 Hooks.on(SimpleCalendar.Hooks.DateTimeChange, (data) => {
     console.log(data);
 });
